a042c6808300c834f41453fb4730063358771b95
Hafeed <Hafeed06@gmail.com>
2022-01-26 22:46:47 +0100

    Backend Created, Frontend Created, Express Initiated

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <strong>Basic React App ! </strong>
+      </header>
+    </div>
+  );
+}
+
+export default App;
